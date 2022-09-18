36797d37d3244c9c37a9528378fe12bc2f3d67d6
Guven Karanfil <guven.karanfil@smartface.io>
2022-09-19 01:19:42 +0300

    Create card design `FeedCard`

---
+++ SocialApp/Feed UI/FeedCard.swift
@@ -0,0 +1,62 @@
+//
+//  FeedCard.swift
+//  SocialApp
+//
+//  Created by Güven Karanfil on 18.09.2022.
+//
+
+import SwiftUI
+
+struct FeedCard: View {
+    let geometry: GeometryProxy
+    let feed: FeedItem
+    
+    init(geometry: GeometryProxy, feed: FeedItem) {
+        self.geometry = geometry
+        self.feed = feed
+    }
+    
+    var body: some View {
+            HStack(alignment: .top) {
+                Image(systemName: "square.and.pencil")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(self.feed.name)
+                            .font(.title)
+                            .bold()
+                        Text("@"+self.feed.username)
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Text(self.feed.text)
+                        .lineLimit(3)
+                        .padding(.trailing)
+                    
+                    AsyncImage(url: self.feed.imageURL)
+                        .frame(width: geometry.size.width / 1.5, height: 200)
+                        .clipShape(RoundedRectangle(cornerRadius: 25))
+                        .aspectRatio(contentMode: .fit)
+
+                }
+            }
+    }
+}
+
+struct FeedCard_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReader { geometry in
+            let feed = FeedItem(
+                id: UUID(),
+                name: "a name",
+                username: "ausername",
+                text: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ",
+                imageURL: URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHCj1m8wirjuacH_P9sphVIKKnsY6KSI2Y3A&usqp=CAU")!)
+            FeedCard(geometry: geometry, feed: feed)
+        }
+    }
+}
