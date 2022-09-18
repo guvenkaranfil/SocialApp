6863c4994a4be1eca2a11d9698980797ae8837c8
Guven Karanfil <guven.karanfil@smartface.io>
2022-09-18 14:16:36 +0300

    Delivers unexpected values error when request completes with data, response and error as nil

--- SocialAppTests/URLSessionHTTPClientTests.swift
+++ SocialAppTests/URLSessionHTTPClientTests.swift
@@ -16,11 +16,15 @@ class URLSessionHTTPClient {
     init(session: URLSession = .shared) {
         self.session = session
     }
+    
+    struct UnexpectedValuesRepresentation: Error {}
 
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
             }
         }.resume()
     }
@@ -73,6 +77,25 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         makeSUT().get(from: url) { _ in }
+
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_getFromURL_failsOnAllNilValues() {
+        URLProtocolStub.stub(data: nil, response: nil, error: nil)
+
+        let exp = expectation(description: "Wait for completion")
+
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case .failure:
+                break
+            default:
+                XCTFail("Expected failure, got \(result) instead")
+            }
+
+            exp.fulfill()
+        }
 
         wait(for: [exp], timeout: 1.0)
     }
