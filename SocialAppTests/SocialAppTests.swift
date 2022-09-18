ff1883f2b228a440687b50e37b0e29e90a585790
Guven Karanfil <guven.karanfil@smartface.io>
2022-09-18 11:21:56 +0300

    `makeSUT` helper function

--- SocialAppTests/SocialAppTests.swift
+++ SocialAppTests/SocialAppTests.swift
@@ -29,18 +29,15 @@ protocol HTTPClient {
 class SocialAppTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        let url = URL(string: "https://any-url.com")!
-        let _ = RemoteFeedLoader(client: client, url: url)
+        let (_, client) = makeSUT()
         
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClientSpy()
         let url = URL(string: "https://any-url.com")!
-        let sut = RemoteFeedLoader(client: client, url: url)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
@@ -49,6 +46,13 @@ class SocialAppTests: XCTestCase {
     
     
     // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://any-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client, url: url)
+        
+        return (sut, client)
+    }
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
