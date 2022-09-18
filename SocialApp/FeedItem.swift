e2cb9fed5ec6f9ec392147cb83314a9b1e194965
Guven Karanfil <guven.karanfil@smartface.io>
2022-09-18 11:06:40 +0300

    Create FeedItem Model

---
+++ SocialApp/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  SocialApp
+//
+//  Created by Güven Karanfil on 18.09.2022.
+//
+
+import Foundation
+
+public struct FeedItem {
+    let id: UUID
+    let name: String
+    let username: String
+    let text: String
+    let imageURL: URL
+}
