6cc648e02d2b23917849ac7b339cc24651b8ad36
Guven Karanfil <guven.karanfil@smartface.io>
2022-09-18 14:29:41 +0300

    Make URLSessionHTTPClient conform to the HTTPClient protocol

--- SocialAppTests/URLSessionHTTPClientTests.swift
+++ SocialAppTests/URLSessionHTTPClientTests.swift
@@ -10,7 +10,7 @@ import Foundation
 import XCTest
 import SocialApp
 
-class URLSessionHTTPClient {
+class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
 
     init(session: URLSession = .shared) {
@@ -106,7 +106,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> URLSessionHTTPClient {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
