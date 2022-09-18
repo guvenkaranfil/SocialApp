a7b5c42ee56f044b814ea09bfa3f7b6493300eda
Guven Karanfil <guven.karanfil@smartface.io>
2022-09-18 11:00:04 +0300

    Initial Commit

---
+++ SocialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialApp
+//
+//  Created by Güven Karanfil on 18.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialApp/SocialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAppApp.swift
+//  SocialApp
+//
+//  Created by Güven Karanfil on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SocialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
